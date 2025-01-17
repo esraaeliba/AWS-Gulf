a55a7e50c03937e30c652ceb0075c87df364de05
esraaeliba <esraaeliba@google.com>
2025-01-17 21:29:21 +0200

    added the project sturcture, packages and libraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(468%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 20s linear infinite',
+      },
+      colors:{
+        primary:"#003540",
+        text:{
+          1:"#333333",
+          2:"#666666",
+
+        }
+      }
+    },
   },
   plugins: [],
 }
