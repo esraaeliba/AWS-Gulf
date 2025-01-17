8d1d9550a29566e6df9e5391a92da1c20dbe37a5
esraaeliba <esraaeliba@google.com>
2025-01-17 21:32:19 +0200

    added services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='flex px-[48px] flex-col justify-center gap-[16px]'>
+            <p className="text-primary text-right font-montserrat text-[36px] font-bold leading-normal">
+                خدماتنا
+            </p>
+            <p className="text-[#666] text-right font-montserrat text-[20px] font-normal leading-[30px] mb-20">
+            يسعدنا أن نقدم أنفسنا كمزود حلول مستقل لأهم العلامات التجارية العالمية المتعلقة بصناعة تكنولوجيا المعلومات وتطوير البرمجيات
+            </p>
+
+        </div>
+    );
+};
+
+export default Services;
